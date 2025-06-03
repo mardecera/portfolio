636f4504869bb299d5571618e3e433754d69e3af
mardecera <18353569+mardecera@users.noreply.github.com>
2025-06-03 11:59:48 -0500

    feat: add new changes

--- src/constants/skills.ts
+++ src/constants/skills.ts
@@ -1,64 +1,23 @@
 export const skills = [
 	{
-		icon: "GithubOutline",
-		title: "Frontend Development",
+		icon: "Front",
+		title: "Desarrollo Frontend",
 		description:
-			"Creating responsive and interactive user interfaces with modern frameworks and libraries.",
-		tecnologies: ["HTML5", "CSS3", "JavaScript", "React", "Next.js", "Astro"],
+			"Creando interfaces de usuario interactivas y responsive con frameworks y librerías modernas.",
+		tecnologies: ["HTML5", "CSS3", "JavaScript", "React", "Next.js", "Astro", "Ant Design"],
 	},
 	{
-		icon: "GithubOutline",
-		title: "Backend Development",
+		icon: "Responsive",
+		title: "Diseño Responsive",
 		description:
-			"Building robust server-side applications and APIs to power web applications.",
-		tecnologies: [
-			"Node.js",
-			"Express.js",
-			"MongoDB",
-			"PostgreSQL",
-			"RESTful APIs",
-		],
+			"Creando sitios web que funcionan de manera óptima en todos los dispositivos y tamaños de pantalla.",
+		tecnologies: ["Mobile First", "Flexbox", "CSS Grid", "Media Queries"],
 	},
 	{
-		icon: "GithubOutline",
-		title: "Full Stack Development",
+		icon: "Tools",
+		title: "Herramientas & Flujos de Trabajo",
 		description:
-			"Integrating frontend and backend technologies to create complete web applications.",
-		tecnologies: ["MERN Stack", "TypeScript", "Vercel"],
-	},
-	{
-		icon: "GithubOutline",
-		title: "Web Performance Optimization",
-		description:
-			"Optimizing websites for speed, accessibility, and search engine visibility.",
-		tecnologies: [
-			"Lighthouse",
-			"Web Vitals",
-			"SEO",
-			"Accessibility",
-			"Performance",
-			"Analytics",
-		],
-	},
-	{
-		icon: "GithubOutline",
-		title: "Responsive Design",
-		description:
-			"Creating websites that work seamlessly across all devices and screen sizes.",
-		tecnologies: [
-			"Mobile First",
-			"Flexbox",
-			"CSS Grid",
-			"Media Queries",
-			"Adaptive UI",
-			"Cross-Browser",
-		],
-	},
-	{
-		icon: "GithubOutline",
-		title: "Tools & Workflows",
-		description:
-			"Utilizing modern development tools and practices for efficient workflow.",
-		tecnologies: ["Git", "GitHub", "VS Code", "Webpack", "Jest", "CI/CD"],
+			"Utilizando herramientas modernas y prácticas para un flujo de trabajo eficiente.",
+		tecnologies: ["Git", "GitHub", "VS Code", "CI/CD", "Scrum", "Jira"],
 	},
 ]
