fb7417d13ff169497cd09fafcacafcafe83c36ac
mardecera <18353569+mardecera@users.noreply.github.com>
2025-09-04 23:38:43 -0500

    chore: reorganize folders

--- src/constants/ui.ts
+++ src/constants/ui.ts
@@ -1,4 +1,4 @@
-import type { IconSize } from "@/components/types/constants/ui"
+import type { IconSize } from "@/types/constants/contants"
 
 export const ICON_SIZES: Record<IconSize, number> = {
 	small: 12,
