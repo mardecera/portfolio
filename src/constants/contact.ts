a8aa1b0f1388eb953466a7695960830a29c50cbd
mardecera <18353569+mardecera@users.noreply.github.com>
2025-09-08 21:34:26 -0500

    fix: link to linkedin

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -2,4 +2,21 @@ export const CONTACT = {
 	email: "mardecera.personal@gmail.com",
 	phoneNumber: "977156464",
 	address: "Arequipa, Perú",
+	social: {
+		linkedin: "https://www.linkedin.com/in/mardecerax/",
+		github: "https://github.com/mardecera",
+	},
+	phones: {
+		pe: {
+			countryCode: "51",
+			phoneNumber: "977156464",
+		},
+	},
+	whatsapp: {
+		baseLink: "https://wa.me/",
+		pe: {
+			message:
+				"Hola, ¿qué tal? Estoy interesado en trabajar contigo en un proyecto. 🚀",
+		},
+	},
 }
